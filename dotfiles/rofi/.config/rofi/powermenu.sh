fd39c8d819a8e739df312f76b5dcb6a6890f6597
RP <info@mindartcode.com>
2026-02-28 23:05:08 +0100

    added swayidle config, changed rofi power menu for idle lock toggle

--- dotfiles/rofi/.config/rofi/powermenu.sh
+++ dotfiles/rofi/.config/rofi/powermenu.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+if pgrep swayidle > /dev/null; then
+    idle_toggle="󰅶 Idle Lock: ON"
+else
+    idle_toggle="󰾪 Idle Lock: OFF"
+fi
+
 # Rofi Power Menu
-options="󰐥 Shutdown\n󰜉 Reboot\n󰤄 Hibernate\n󰒲 Lock"
+options="󰐥 Shutdown\n󰜉 Reboot\n󰤄 Hibernate\n󰒲 Lock\n${idle_toggle}"
 
 chosen=$(echo -e "$options" | rofi -dmenu -i -p "Power Menu" -theme-str 'window {width: 250px;}')
 
@@ -17,5 +23,14 @@ case $chosen in
         ;;
     *Lock)
         swaylock -c 000000 
+        ;;
+    *"Idle Lock"*)
+        if pgrep swayidle > /dev/null; then
+            pkill swayidle
+            notify-send "Idle Lock" "Disabled"
+        else
+            swayidle -C ~/.config/swayidle/config &
+            notify-send "Idle Lock" "Enabled"
+        fi
         ;;
 esac
