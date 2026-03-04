cb54ea207b56ceb1641cbce8d464a75730bd154d
Robin Proeckl <71661183+RoProe@users.noreply.github.com>
2026-03-04 19:17:12 +0100

    Add 'touch' to installed multiple commands

--- dracut/99snapshot-menu/module-setup.sh
+++ dracut/99snapshot-menu/module-setup.sh
@@ -4,5 +4,5 @@ depends() { echo "btrfs"; }
 install() {
     inst_hook initqueue/settled 50 "$moddir/snapshot-menu.sh"
     inst_hook pre-mount         10 "$moddir/apply-rootflags.sh"
-    inst_multiple btrfs awk sed cat find mount umount
+    inst_multiple btrfs awk sed cat find mount umount touch
 }
