b6737817ed24292b858aa3f0f35c35b7dd1379a4
RP <info@mindartcode.com>
2026-04-27 23:52:10 +0200

    restore pre-mount hook and dracut-pre-mount.service ordering for snapshot menu

--- install/dracut/99snapshot-menu/module-setup.sh
+++ install/dracut/99snapshot-menu/module-setup.sh
@@ -2,7 +2,7 @@
 check()   { return 0; }
 depends() { echo "btrfs"; }
 install() {
-    inst_hook initqueue/finished 90 "$moddir/snapshot-menu.sh"
+    inst_hook pre-mount 05 "$moddir/snapshot-menu.sh"
     inst_script "$moddir/snapshot-rewrite.sh" /usr/bin/snapshot-rewrite
     inst_simple "$moddir/snapshot-rewrite.service" /usr/lib/systemd/system/snapshot-rewrite.service
     inst_multiple btrfs awk sed cat find mount umount touch sort head cut tee systemctl lsblk blkid udevadm
